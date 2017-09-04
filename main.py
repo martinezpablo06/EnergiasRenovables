c88787a97414f7fca5a4ee52612cffe3e48caf45
Miguel Jimeno <migueljimeno.p@gmail.com>
2017-09-04 07:27:39 +0000

    Ultima actualizacion estable en RaspberryPi, modificando los dos archivos y agregando el .gitignore, faltando reconocer el ultimo cambio de estado antes de cerrar, no funciona la pantalla y falta deshabilitar los botones de la pantalla.

--- main.py
+++ main.py
@@ -16,80 +16,79 @@ from kivy.graphics import Color, Rectangle
 
 
 
-arduino = serial.Serial('/dev/ttyUSB0', baudrate=9600)
+arduino = serial.Serial('/dev/ttyUSB1', baudrate=9600)
 #arduino.open()
 
 swState = "000"
-
+buttonPin = 12 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 # Define some helper functions:
 
-# This callback will be bound to the LED toggle and Beep button:
+
 def press_callback(obj):
-	arduino.write('0')	
-	arduino.flush()
-        auxswState = ""
-	time.sleep(0.1)
-	while arduino.inWaiting()>0:
-		auxswState+=arduino.read(1)
-	swState = auxswState
 	if obj.text == 'button1':		
 		if(swState[0] == '0'):
 			obj.state = "normal"
 		elif(swState[0] == '1'):
-			#obj.disabled = not obj.disabled 
 			if(obj.state == "normal"):
 				print ("button 1 off")
 				print (swState)
 				arduino.write('2')
-				#GPIO.output(ledPin, GPIO.LOW)
 			elif(obj.state == "down"):
 				print ("button 1 on")
 				print (swState)
 				arduino.write('1')
-				#GPIO.output(ledPin, GPIO.HIGH)
 
 	if obj.text == 'button2':		
 		if(swState[1] == '0'):
 			obj.state = "normal"
 		elif(swState[1] == '1'):
-			#obj.disabled = not obj.disabled 
 			if(obj.state == "normal"):
 				print ("button 2 off")
 				print (swState)
 				arduino.write('12')
-				#GPIO.output(ledPin, GPIO.LOW)
 			elif(obj.state == "down"):
 				print ("button 2 on")
 				print (swState)
 				arduino.write('11')
-				#GPIO.output(ledPin, GPIO.HIGH)
 	
 	if obj.text == 'button3':		
 		if(swState[2] == '0'):
 			obj.state = "normal"
 		elif(swState[2] == '1'):
-			#obj.disabled = not obj.disabled 
 			if(obj.state == "normal"):
 				print ("button 3 off")
 				print (swState)
 				arduino.write('48')
-				#GPIO.output(ledPin, GPIO.LOW)
 			elif(obj.state == "down"):
 				print ("button 3 on")
 				print (swState)
 				arduino.write('47')
-				#GPIO.output(ledPin, GPIO.HIGH)
-
-bti = InputButton(text="bti")
-        Clock.schedule_interval(bti.update, 1.0/10.0)
-        layout.add_widget(bti)
 
 class InputButton(Button):
     def update(self, dt):
-        if GPIO.input(buttonPin) == True:
-            self.state = 'normal'
-        else:
-            self.state = 'down'
+		if GPIO.input(buttonPin) == True:
+			self.state = 'normal'
+		else:
+			self.state = 'down'
+			#arduino.flush()
+			arduino.write('99')
+			auxswState = ""
+			#time.sleep(0.1)
+			while arduino.inWaiting()>0:
+				auxswState+=arduino.read(1)
+			if (len(auxswState) >= 3):
+				global swState
+				swState = auxswState
+				print (swState)
+				'''
+				val = int (auxswState)			
+				print (type(val))
+
+				print val
+				'''
+			time.sleep(0.1)
 
 class MyApp(App):
 
@@ -102,9 +101,6 @@ class MyApp(App):
 			Color(.2,.2,.2,1)
 			self.rect = Rectangle(size=(1920,1080), pos=layout.pos)
             
-            bti = InputButton(text="bti")
-            Clock.schedule_interval(bti.update, 1.0/10.0)
-            layout.add_widget(bti)
 
 		button1 = ToggleButton(text="button1")
 		button1.bind(on_press=press_callback)
@@ -114,6 +110,10 @@ class MyApp(App):
 
 		button3 = ToggleButton(text="button3")
 		button3.bind(on_press=press_callback)
+
+		bti = InputButton(text="bti")
+		Clock.schedule_interval(bti.update, 1.0/10.0)
+			#layout.add_widget(bti)
 
 		layout.add_widget(button1)
 		layout.add_widget(button2)
--- modelandled/modelandled.ino
+++ modelandled/modelandled.ino
@@ -1,8 +1,9 @@
   int switch1 = 2;    
   int switch2 = 3;   
-  int switch3 = 4;    
+  int switch3 = 4; 
+  int rbpin = 5;     
   
-  int relePins[] = { 0, 5, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 
+  int relePins[] = { 0, 8, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 
                      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7 };
   
   int arrayState[]  = {0,0,0};
@@ -10,6 +11,8 @@
   int sw1 = 0;
   int sw2 = 0;
   int sw3 = 0;
+
+  int atr = 0; 
 
   void setup() {
     
@@ -21,11 +24,29 @@
     
     pinMode(5, OUTPUT);   
     pinMode(6, OUTPUT);   
-    pinMode(7, OUTPUT);       
+    pinMode(7, OUTPUT); 
+
+    pinMode(rbpin, OUTPUT);  
+    digitalWrite(rbpin, HIGH);
     
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(7, LOW);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
+    digitalWrite(7, HIGH);
+
+    arrayState[0] = 0;
+    arrayState[1] = 0;
+    arrayState[2] = 0;
+    
+    sw1 = digitalRead(switch3);
+    sw2 = digitalRead(switch2);
+    sw3 = digitalRead(switch1);
+
+    Serial.print(sw1);
+    Serial.print(sw2);
+    Serial.print(sw3);
+        
+    int atr = 0; 
+    
   }
         
      
@@ -36,33 +57,55 @@
     sw2 = digitalRead(switch2);
     sw3 = digitalRead(switch1);
 
-    arrayState[0] = sw1;
-    arrayState[1] = sw2;
-    arrayState[2] = sw3;    
+    delay(200);
+
+    if ( arrayState[0] != sw1){
+       arrayState[0] = sw1;
+       atr = 1;
+    }
+
+    if (arrayState[1]!= sw2){
+       arrayState[1] = sw2;
+       atr = 1;
+   }
+   
+    if (arrayState[2]!= sw3){
+       arrayState[2] = sw3;
+       atr = 1;  
+   }
+   
+    if(atr == 1){ 
+        digitalWrite(rbpin,LOW);
+        delay(100);
+        digitalWrite(rbpin,HIGH);
+        atr = 0;
+    }
     
     while(Serial.available() > 0){
       char ch = Serial.read();
-      if (isDigit(ch)) { 
+      if (isDigit(ch)) { //Filtrado de entrada solo deja numeros 0 al 9
         codigo += ch;
       } 
     } 
-    
     int data = codigo.toInt();
 
-    if(data == 0){
+    if(data == 99){ 
+        digitalWrite(rbpin,HIGH);
         Serial.print(arrayState[0]);
         Serial.print(arrayState[1]);
         Serial.print(arrayState[2]);
-        Serial.println();  
-    }else if (data > 0){
+        //Serial.println();
+        
+ 
+    } else if (data < 99){
       if (data % 2 == 0){
         int auxpar = (data/2);
-        digitalWrite(relePins[auxpar],LOW); 
+        digitalWrite(relePins[auxpar],HIGH); 
       }
       else if (data % 2 != 0) {
         int auximpar = ((data + 1)/2);
-        digitalWrite(relePins[auximpar],HIGH);
+        digitalWrite(relePins[auximpar],LOW);
       }  
     }
-    delay(200);
+ 
   }
